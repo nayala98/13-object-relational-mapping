648762497a9d95181a93f376d0caecfd7c10fa5a
nayala <nathaniel.ayala20@gmail.com>
2021-06-20 22:28:01 -0500

    Fixed some errors

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,12 +6,20 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
-  Tag.findAll()
-        .then(dbTagData => res.json(dbTagData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+  Tag.findAll({
+      attributes: ['id', 'tag_name'],
+      include: [
+          {
+              model: Product,
+              attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+          }
+      ]
+  })
+      .then(dbTagData => res.json(dbTagData))
+      .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+      });
 });
 
 router.get('/:id', (req, res) => {
@@ -20,7 +28,14 @@ router.get('/:id', (req, res) => {
   Tag.findOne({
     where: {
         id: req.params.id
-    }
+    },
+    attributes: ['id', 'tag_name'],
+    include: [
+        {
+            model: Product,
+            attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+        }
+    ]
 })
     .then(dbTagData => {
         if (!dbTagData) {
